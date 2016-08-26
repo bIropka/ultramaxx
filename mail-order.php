66eebadbc83bf11d3746ded84d3f55ac34d788a4
bIropka <BrDm_100>
2016-08-26 04:43:55 +0300

    v 0.9

---
+++ mail-order.php
@@ -0,0 +1,30 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['name-1'])) {$name = $_POST['order-name'];}
+    if (isset($_POST['phone-1'])) {$phone = $_POST['order-tel'];}
+    if (isset($_POST['phone-1'])) {$country = $_POST['order-country'];}
+    if (isset($_POST['phone-1'])) {$sum = $_POST['order-full-cost'];}
+    if (isset($_POST['formData'])) {$formData = $_POST['formData'];}
+
+    $to = "biropka@gmail.com"; /*Укажите адрес, на который должно приходить письмо*/
+    $sendfrom   = "ultramaxx"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
+    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
+    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
+    $subject = "$formData";
+    $message = "$formData <br><b>Поступил заказ </b> <br> <b>Имя клиента:</b> $name <br><b>Телефон:</b> $phone <br> <b>Страна:</b> $country <br> <b>Сумма заказа:</b> $sum ";
+    $send = mail ($to, $subject, $message, $headers);
+    if ($send == 'true')
+    {
+    echo '<p class="success">Спасибо за обращение к нам!</p>';
+    }
+    else 
+    {
+    echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
+    }
+} else {
+    http_response_code(403);
+    echo "Попробуйте еще раз";
+}
+?>
